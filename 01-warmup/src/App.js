5f73f5e3ca4e75756ef5017556c667c5c1ddd3ed
sail-amir <edu.ebrahimi@gmail.com>
2021-10-11 13:21:56 -0400

    Add expense date component and css styles

--- 01-warmup/src/App.js
+++ 01-warmup/src/App.js
@@ -1,15 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
+  const expenses = [
+    { title: "car insurance", amount: 234, date: new Date(2012, 2, 24) },
+    { title: "house insurance", amount: 234, date: new Date(2015, 5, 2) },
+    { title: "appliances insurance", amount: 234, date: new Date(2018, 2, 13) },
+    { title: "grocery", amount: 234, date: new Date(2020, 3, 10) },
+  ];
   return (
     <div className="App">
       <h1>Let's start programming</h1>
       <p>This is my first react!</p>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
     </div>
   );
-}
+};
 
 export default App;
